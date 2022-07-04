3dc306dd5fbde6430bfa74cbc456b2d63c8620dc
Elionas Fernando <79639523+Elionas-dev@users.noreply.github.com>
2022-07-04 15:43:23 -0300

    Update 1035.js

--- exercicios_beeCrowd/1035.js
+++ exercicios_beeCrowd/1035.js
@@ -6,21 +6,14 @@ let [A, B, C, D] = linhas.map(item => parseInt(item));
 let soma1 = C + D;
 let soma2 = A + B;
 
-if (B > C && D > A) {
-  if (soma1 > soma2) {
-    if (C > 0 && D > 0) {
-      A = A % 2;
-      if ((A === 0)) {
-        console.log("Valores aceitos");
-      }else {
-        console.log("Valores nao aceitos");
-      }
-    }else {
-        console.log("Valores nao aceitos");
-      }
-  }else {
-    console.log("Valores nao aceitos");
-  }
-} else {
-  console.log("Valores nao aceitos");
-}
+let result = 'Valores nao aceitos';
+
+
+if (B > C && D > A)
+  if (soma1 > soma2)
+    if (C > 0 && D > 0)
+     if (A % 2 === 0) 
+      result = "Valores aceitos";
+
+ 
+console.log(result);
