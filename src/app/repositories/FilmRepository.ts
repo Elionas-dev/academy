38c34afbd81eed5cfdf63110a9a3396d13b69c51
Elionas Fernando <79639523+Elionas-dev@users.noreply.github.com>
2022-07-06 15:05:21 -0300

    Delete src directory

--- src/app/repositories/FilmRepository.ts
+++
@@ -1,5 +0,0 @@
-import model from '../models/FilmModel';
-
-export const getAll = () => {
-   return model.findAll();
-};
--- src/app/services/FilmService.ts
+++
@@ -1,5 +0,0 @@
-import * as repository from '../repositories/FilmRepository';
-
-export const getAll = () => {
-   return repository.getAll();
-};
