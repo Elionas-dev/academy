c54ec9cbe860fe2208c342f1acb7b13b8a148df0
Elionas Fernando <79639523+Elionas-dev@users.noreply.github.com>
2022-07-07 13:59:50 -0300

    Add files via upload

---
+++ typeScript 2.0/src/app.ts
@@ -0,0 +1,22 @@
+import express, {Express, Request, Response} from 'express';
+import connection from './database/sequelize';
+import bodyParser from 'body-parser';
+
+import films from './app/controllers/FilmController';
+
+const app: Express = express();
+const port: number = 3333;
+
+app.use(bodyParser.json());
+
+app.use('/films', films);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world Express + TypeScript!!!');
+});
+
+app.listen(3333, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+connection();
